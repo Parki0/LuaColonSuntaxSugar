ed492b12d12ee84d940cdb785263c24eeee95369
Parki <parki06780678@gmail.com>
2016-01-24 06:02:46 +1000

    fixed bug with defining tab(tab({}))

--- tab.lua
+++ tab.lua
@@ -1,26 +1,30 @@
 function tab(t)
 	assert(type(t)=='table','Got '..type(t)..' table expected.')
-	if getmetatable(t) == nil then
-		for k,v in pairs(t) do
-			if type(v) == 'table' then
-				t[k] = tab(v)
+	if not isTab(t) then
+		if getmetatable(t) == nil then
+			for k,v in pairs(t) do
+				if type(v) == 'table' then
+					t[k] = tab(v)
+				end
+			end
+			return setmetatable(t,{
+				__index = table,
+				__newindex = function(t,k,v)
+								if type(v) == 'table' and isTab(v) == false then
+									rawset(t,k,tab(v))
+								else
+									rawset(t,k,v)
+								end
+						end,
+				tab = true
+				})
+		else
+			if isTab(t) ~= true then
+				assert(false,"table already has a metatable")
 			end
 		end
-		return setmetatable(t,{
-			__index = table,
-			__newindex = function(t,k,v)
-							if type(v) == 'table' and isTab(v) == false then
-								rawset(t,k,tab(v))
-							else
-								rawset(t,k,v)
-							end
-					end,
-			tab = true
-			})
 	else
-		if isTab(t) ~= true then
-			assert(false,"table already has a metatable")
-		end
+		return t
 	end
 end
 function isTab(t)
